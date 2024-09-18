e7f7895a698c1555acb20fde446d392114e95401
Laetitia Huriaux <laetitia.huriaux@gmail.com>
2024-09-18 04:10:20 -0400

    Module 5 correct lint issues

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
 class CarMake(models.Model):
@@ -12,8 +12,9 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -28,4 +29,4 @@ class CarModel(models.Model):
         ])
 
     def __str__(self):
-        return self.name  
+        return self.name
