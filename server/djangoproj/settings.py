03dee126dd372f47b91cdd9b805c4e4c3233b22e
Laetitia Huriaux <laetitia.huriaux@gmail.com>
2024-09-18 04:49:31 -0400

    Module 5 correct lint issues

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,13 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://laetitiahuri-8000.'+
-    'theiadockernext-1-labs-prod-theiak8s-4-tor01'+
+    'https://laetitiahuri-8000.' +
+    'theiadockernext-1-labs-prod-theiak8s-4-tor01' +
     '.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://laetitiahuri-8000.'+
-    'theiadockernext-1-labs-prod-theiak8s-4-tor01'+
+    'https://laetitiahuri-8000.' +
+    'theiadockernext-1-labs-prod-theiak8s-4-tor01' +
     '.proxy.cognitiveclass.ai'
 ]
 
@@ -103,7 +103,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.' +
+        'auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
